bc9a3a3b5108d9bb800ffa4c66c0f45446f8c21c
Aase Mestad <aase.mestad@difi.no>
2021-01-14 10:24:27 +0100

    .well-known og jwks

---
+++ src/main/java/no/idporten/minidplus/web/OpenIDController.java
@@ -0,0 +1,35 @@
+package no.idporten.minidplus.web;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import no.idporten.sdk.oidcserver.OpenIDConnectIntegration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping(value = "/api")
+@CrossOrigin(origins = "*")
+@AllArgsConstructor
+@NoArgsConstructor
+public class OpenIDController {
+
+    @Autowired
+    OpenIDConnectIntegration openIDConnectIntegration;
+
+    @RequestMapping(value = "/.well-known/openid-configuration", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public ResponseEntity openIDConfiguration() {
+        return ResponseEntity.ok().body(openIDConnectIntegration.getOpenIDProviderMetadata());
+    }
+
+    @RequestMapping(value = "/jwks", produces = "application/json")
+    @ResponseBody
+    public ResponseEntity jwks() {
+        return ResponseEntity.ok().body(openIDConnectIntegration.getPublicJWKSet());
+    }
+}
